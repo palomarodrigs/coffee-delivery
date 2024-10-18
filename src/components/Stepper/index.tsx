c1587f405210900b22654ee9bf6e95a976a51f87
palomarodrigs <palomarodrigues.0013@gmail.com>
2024-10-18 17:14:15 -0300

    feat: add increment and decrement to Stepper component

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react'
+
 import { Minus, Plus } from 'phosphor-react'
 import { StepperContainer } from './styles'
 
 export function Stepper() {
+  const [value, setValue] = useState(1)
+
+  const increment = () => setValue((prevValue) => prevValue + 1)
+  const decrement = () => {
+    if (value > 0) {
+      setValue((prevValue) => prevValue - 1)
+    }
+  }
+
   return (
     <StepperContainer>
-      <button>
+      <button onClick={decrement}>
         <Minus size={14} />
       </button>
 
-      <span>1</span>
+      <span>{value}</span>
 
-      <button>
+      <button onClick={increment}>
         <Plus size={14} />
       </button>
     </StepperContainer>
